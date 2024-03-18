2ff692d17c8b31184dcc3e6fed36480e3dcf251b
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2024-03-18 10:15:00 +0100

    12.2

--- hellooop1/oopopdracht11/Program.cs
+++ hellooop1/oopopdracht11/Program.cs
@@ -6,16 +6,8 @@ namespace ziescherper_11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hoofdstuk 11");
-            ziescherper_10.classes.Student  student= new Student();
             
-            ziescherper_10.classes.Student  student1= new Student("papaya");
-            Console.WriteLine(student.Naam);
-            Console.WriteLine(student1.Naam);
-
-            ziescherper_10.classes.Student[] students= new Student[] {student,student1};
-            students[1].Naam = "petronella";
-            Console.WriteLine(student1.Naam);
         }
+        
     }
 }
--- hellooop1/ziescherper12/Program.cs
+++ hellooop1/ziescherper12/Program.cs
@@ -7,7 +7,54 @@ namespace ziescherper_12
         static void Main(string[] args)
         {
             Console.WriteLine("hoofdstuk12");
+            Opdracht12_1();
+
+            List<int> alleGetallen = new List<int>();
+            alleGetallen.Add(69);
+            alleGetallen.Add(420);
+            List<bool> binaryList = new List<bool>();
+            binaryList.Add(true);
+            binaryList.Add(false);
+            List<Student> pokeDex = new List<Student>();
+            Student pikachu = new Student("pikachu");
+ 
+            pokeDex.Add(pikachu);
+            Student charmander = new Student("charmander");
+            pokeDex.Add(charmander);
+            List<string[]> listOfStringarrays = new List<string[]>();
+
+
+            for (int i = 0; i < pokeDex.Count; i++)
+            {
+                Console.WriteLine(pokeDex[i].Naam);
+                Console.WriteLine(pokeDex[i].geboortejaar);
+
+            }
+            for (int i = 0; i < alleGetallen.Count; i++)
+            {
+                Console.WriteLine(alleGetallen[i]);
+            }
+            for (int i = 0; i < binaryList.Count; i++)
+            {
+                Console.WriteLine(binaryList[i]);
+            }
 
         }
+        private static void Opdracht12_1()
+        {
+            Console.WriteLine("12.1");
+
+            Student student = new Student();
+
+            Student student1 = new Student("papaya");
+            Console.WriteLine(student.Naam);
+            Console.WriteLine(student1.Naam);
+
+            Student[] students = new Student[] { student, student1 };
+            students[1].Naam = "petronella";
+            Console.WriteLine(student1.Naam);
+        }
+
+
     }
 }
