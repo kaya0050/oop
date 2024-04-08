e2b1b42bf9a6871b8a525151d292564c5064455d
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2024-04-08 13:15:44 +0200

    12.4

--- hellooop1/ziescherper12/Program.cs
+++ hellooop1/ziescherper12/Program.cs
@@ -1,14 +1,50 @@
 ﻿using ziescherper_10.classes;
 namespace ziescherper_12
-
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("hoofdstuk12");
             Opdracht12_1();
+            Opdracht12_2();
+            Opdracht12_3();
+            Queue<string> wachtrij = new Queue<string>();
+            wachtrij.Enqueue("Ik stond hier eerste.");
+            wachtrij.Enqueue("Ik tweedes.");
+            wachtrij.Enqueue("Ik laatste.");
 
+            Console.WriteLine(wachtrij.Dequeue());
+            Console.WriteLine(wachtrij.Dequeue());
+            Console.WriteLine(wachtrij.Dequeue());
+
+            Stack<string> stapel = new Stack<string>();
+            stapel.Push("Ik was eerste hier.");
+            stapel.Push("Ik tweede.");
+            stapel.Push("Ik als laatste.");
+
+            Console.WriteLine(stapel.Pop());
+            Console.WriteLine(stapel.Pop());
+
+        }
+        private static void Opdracht12_1()
+        {
+            Console.WriteLine("12.1");
+
+            Student student = new Student();
+
+            Student student1 = new Student("papaya");
+            Console.WriteLine(student.Naam);
+            Console.WriteLine(student1.Naam);
+
+            Student[] students = new Student[] { student, student1 };
+            students[1].Naam = "petronella";
+            Console.WriteLine(student1.Naam);
+        }
+
+        private static void Opdracht12_2()
+        {
             List<int> alleGetallen = new List<int>();
             alleGetallen.Add(69);
             alleGetallen.Add(420);
@@ -17,7 +53,7 @@ namespace ziescherper_12
             binaryList.Add(false);
             List<Student> pokeDex = new List<Student>();
             Student pikachu = new Student("pikachu");
- 
+
             pokeDex.Add(pikachu);
             Student charmander = new Student("charmander");
             pokeDex.Add(charmander);
@@ -38,23 +74,26 @@ namespace ziescherper_12
             {
                 Console.WriteLine(binaryList[i]);
             }
-
         }
-        private static void Opdracht12_1()
+
+        private static void Opdracht12_3()
         {
-            Console.WriteLine("12.1");
+            double[] killDeathRates = { 1, 0.89, 3.15, 0.1 };
+            foreach (double singleKD in killDeathRates)
+            {
+                Console.WriteLine(singleKD);
+            }
+            Student timmy = new Student();
+            timmy.Naam = "tim";
+            Student tommy = new Student();
+            tommy.Naam = "tom";
+            Student[] deklas = new Student[] { timmy, tommy };
 
-            Student student = new Student();
-
-            Student student1 = new Student("papaya");
-            Console.WriteLine(student.Naam);
-            Console.WriteLine(student1.Naam);
-
-            Student[] students = new Student[] { student, student1 };
-            students[1].Naam = "petronella";
-            Console.WriteLine(student1.Naam);
+            foreach (var eenStudent in deklas)
+            {
+                Console.WriteLine($"{eenStudent.Naam}, {eenStudent.geboortejaar}");
+            }
         }
-
-
+            
     }
 }
