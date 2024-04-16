c4d68638b31053a573af2f0ded599422096efa78
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2024-04-16 12:26:08 +0200

    push

--- hellooop1/ziescherper12/Program.cs
+++ hellooop1/ziescherper12/Program.cs
@@ -10,22 +10,7 @@ namespace ziescherper_12
             Opdracht12_1();
             Opdracht12_2();
             Opdracht12_3();
-            Queue<string> wachtrij = new Queue<string>();
-            wachtrij.Enqueue("Ik stond hier eerste.");
-            wachtrij.Enqueue("Ik tweedes.");
-            wachtrij.Enqueue("Ik laatste.");
-
-            Console.WriteLine(wachtrij.Dequeue());
-            Console.WriteLine(wachtrij.Dequeue());
-            Console.WriteLine(wachtrij.Dequeue());
-
-            Stack<string> stapel = new Stack<string>();
-            stapel.Push("Ik was eerste hier.");
-            stapel.Push("Ik tweede.");
-            stapel.Push("Ik als laatste.");
-
-            Console.WriteLine(stapel.Pop());
-            Console.WriteLine(stapel.Pop());
+            Opdracht12_4();
 
         }
         private static void Opdracht12_1()
@@ -94,6 +79,41 @@ namespace ziescherper_12
                 Console.WriteLine($"{eenStudent.Naam}, {eenStudent.geboortejaar}");
             }
         }
-            
+        
+        private static void Opdracht12_4()
+        {
+            Queue<string> wachtrij = new Queue<string>();
+            wachtrij.Enqueue("Ik stond hier eerste.");
+            wachtrij.Enqueue("Ik tweedes.");
+            wachtrij.Enqueue("Ik laatste.");
+
+            Console.WriteLine(wachtrij.Dequeue());
+            Console.WriteLine(wachtrij.Dequeue());
+            Console.WriteLine(wachtrij.Dequeue());
+
+            Stack<string> stapel = new Stack<string>();
+            stapel.Push("Ik was eerste hier.");
+            stapel.Push("Ik tweede.");
+            stapel.Push("Ik als laatste.");
+
+            Console.WriteLine(stapel.Pop());
+            Console.WriteLine(stapel.Pop());
+
+            Dictionary<int, string> klanten = new Dictionary<int, string>();
+            klanten.Add(123, "Tim pim");
+            klanten.Add(6463, "James Kont");
+            klanten.Add(666, "mr beast");
+            klanten.Add(700, "James Kont");
+            Console.WriteLine(klanten[123]);
+            foreach (var item in klanten)
+            {
+                Console.WriteLine(item.Key + "\t:" + item.Value);
+            }
+            Dictionary<string, Student> studentenLijst = new Dictionary<string, Student>();
+            Student stud = new Student() { Naam = "Tim", geboortejaar = 2001 };
+            studentenLijst.Add("AB12", stud);
+            Console.WriteLine(studentenLijst["AB12"].geboortejaar);
+            studentenLijst.Remove("AB12");
+        }
     }
 }
