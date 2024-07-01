fe466a3097f394f49e4851b48b407954e5528af6
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2024-07-01 10:04:32 +0200

    commit

--- hellooop1/hellooop1/obj/Debug/net6.0/hellooop1.AssemblyInfo.cs
+++ hellooop1/hellooop1/obj/Debug/net6.0/hellooop1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hellooop1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d68638b31053a573af2f0ded599422096efa78")]
 [assembly: System.Reflection.AssemblyProductAttribute("hellooop1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hellooop1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
